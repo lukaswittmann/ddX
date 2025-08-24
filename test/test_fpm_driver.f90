43e9e8b2765e61412279cf5eef8b449611ddb57b
lukaswittmann <mail@lukaswittmann.com>
2025-08-24 14:55:42 +0200

    Make test driver search for wrapper in several relative locations and execute it

--- test/test_fpm_driver.f90
+++ test/test_fpm_driver.f90
@@ -1,8 +1,24 @@
 program test_fpm_driver
   implicit none
-  integer :: istat
-  call execute_command_line('bash ../../../test/fpm-test.sh', exitstat=istat)
-  if (istat /= 0) then
-    stop 1
-  end if
+  integer :: istat, i
+  character(len=256), dimension(6) :: paths
+  character(len=512) :: cmd
+
+  paths = (/'./test/fpm-test.sh','../test/fpm-test.sh','../../test/fpm-test.sh',&
+            '../../../test/fpm-test.sh','../../../../test/fpm-test.sh','/test/fpm-test.sh'/)
+
+  do i = 1, size(paths)
+    call execute_command_line('test -x ' // trim(paths(i)), exitstat=istat)
+    if (istat == 0) then
+      cmd = 'bash ' // trim(paths(i))
+      call execute_command_line(trim(cmd), exitstat=istat)
+      if (istat /= 0) then
+        stop 1
+      end if
+      stop 0
+    end if
+  end do
+
+  ! If we reach here no wrapper found
+  stop 1
 end program test_fpm_driver
